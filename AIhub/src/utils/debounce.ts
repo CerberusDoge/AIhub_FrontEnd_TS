2e45ad90ab666d8ef2b54c1b6b546169ea0f1ac6
郭翔宇 <1499452723@qq.com>
2025-03-10 18:51:58 +0800

    feat: 完成登录注册功能

--- AIhub/src/utils/debounce.ts
+++ AIhub/src/utils/debounce.ts
@@ -2,7 +2,7 @@ export  const debounce = (func: any, delay: number) => {
   let timer: any = null
   return (...args: any) => {
     const context = this
-    if (timer) clearTimeout(timer)
+    clearTimeout(timer)
     timer = setTimeout(() => func.apply(context, args), delay)
   }
 }
