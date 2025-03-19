3e656ec6479a3dec692173249043300b33c725a7
郭翔宇 <1499452723@qq.com>
2025-03-19 21:16:54 +0800

    修改分包策略

--- vite.config.ts
+++ vite.config.ts
@@ -58,13 +58,13 @@ export default defineConfig({
         chunkFileNames: 'js/[name]-[hash].js', // 引入文件名的名称
         entryFileNames: 'js/[name]-[hash].js', // 包的入口文件名称
         assetFileNames: '[ext]/[name]-[hash].[ext]', // 资源文件像 字体，图片等
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            // 让每个插件都打包成独立的文件
-            if (id.includes('vue')) return 'vue'
-            else return id.toString().split('node_modules/')[1].split('/')[0].toString()
-          }
-        },
+        // manualChunks(id) {
+        //   if (id.includes('node_modules')) {
+        //     if (id.includes('vue')) return 'vue'
+        //     // 让每个插件都打包成独立的文件
+        //     return id.toString().split('node_modules/')[1].split('/')[0].toString()
+        //   }
+        // },
       },
     },
   },
